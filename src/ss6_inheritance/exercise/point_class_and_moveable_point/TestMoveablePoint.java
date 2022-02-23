a9040ae18b31147ce939b95fdbaa9a7d462d95c0
khoanvuviefox <khoanvuviefox@gmail.com>
2022-02-23 14:39:25 +0700

    [Bài tập] Lớp Point và MoveablePoint

---
+++ src/ss6_inheritance/exercise/point_class_and_moveable_point/TestMoveablePoint.java
@@ -0,0 +1,6 @@
+package ss6_inheritance.exercise.point_class_and_moveable_point;
+public class TestMoveablePoint {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ss6_inheritance/exercise/point_class_and_moveable_point/TestPoint.java
@@ -0,0 +1,8 @@
+package ss6_inheritance.exercise.point_class_and_moveable_point;
+
+public class TestPoint {
+    public static void main(String[] args) {
+        Point point = new Point(3, 5);
+        System.out.println(point.toString());
+    }
+}
