8378889da81de3f39244883425ec4b0de6992eb0
khoanvuviefox <khoanvuviefox@gmail.com>
2022-02-23 13:46:25 +0700

    [Bài tập] Lớp Point2D và lớp Point3D

---
+++ src/ss6_inheritance/exercise/point2d_point3d/TestPint3D.java
@@ -0,0 +1,10 @@
+package ss6_inheritance.exercise.point2d_point3d;
+
+public class TestPint3D {
+    public static void main(String[] args) {
+
+        Point3D first3DPoint=new Point3D(4, 5,7);
+
+        System.out.println(first3DPoint.toString());
+    }
+}
---
+++ src/ss6_inheritance/exercise/point2d_point3d/TestPoint2D.java
@@ -0,0 +1,10 @@
+package ss6_inheritance.exercise.point2d_point3d;
+
+public class TestPoint2D {
+    public static void main(String[] args) {
+
+        Point2D first2DPoint=new Point2D(3, 4);
+
+        System.out.println(first2DPoint.toString());
+    }
+}
