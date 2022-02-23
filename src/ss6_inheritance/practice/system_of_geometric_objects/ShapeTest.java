7c48134f5eeab83d5d71b857f1124669144d1ba1
khoanvuviefox <khoanvuviefox@gmail.com>
2022-02-23 10:58:51 +0700

    Thực hành

---
+++ src/ss6_inheritance/practice/system_of_geometric_objects/ShapeTest.java
@@ -0,0 +1,11 @@
+package ss6_inheritance.practice.system_of_geometric_objects;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+    }
+}
