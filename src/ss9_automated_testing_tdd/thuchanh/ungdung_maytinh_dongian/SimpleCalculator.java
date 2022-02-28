d3e4140d9624ec092e88d01a4a5fffe57c99d8c7
khoanvuviefox <khoanvuviefox@gmail.com>
2022-02-28 08:37:39 +0700

    [Thực hành] Ứng dụng máy tính đơn giản

---
+++ src/ss9_automated_testing_tdd/thuchanh/ungdung_maytinh_dongian/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package ss9_automated_testing_tdd.thuchanh.ungdung_maytinh_dongian;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
