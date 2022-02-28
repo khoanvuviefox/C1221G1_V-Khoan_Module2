f267886ed28408b00bc6fbe38d719775ae972ca1
khoanvuviefox <khoanvuviefox@gmail.com>
2022-02-28 16:33:26 +0700

    [Thực hành] Triển khai Queue sử dụng danh sách liên kết

---
+++ src/ss11_dsa_stack_queue/thuchanh/create_queue_use_class_linkedlist/Node.java
@@ -0,0 +1,11 @@
+package ss11_dsa_stack_queue.thuchanh.create_queue_use_class_linkedlist;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node (int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
