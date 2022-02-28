fd63a3412dbc1925af473fa8e0c8f7f3677195ce
khoanvuviefox <khoanvuviefox@gmail.com>
2022-02-28 15:17:31 +0700

    [Thực hành] Triển khai stack sử dụng lớp LinkedList trong thư viện java.util

---
+++ src/ss11_dsa_stack_queue/thuchanh/create_stack_use_class_linkedlist/GenericStackTesst.java
@@ -0,0 +1,13 @@
+package ss11_dsa_stack_queue.thuchanh.create_stack_use_class_linkedlist;
+
+import static ss11_dsa_stack_queue.thuchanh.create_stack_use_class_linkedlist.MyGenericStack.stackOfIStrings;
+import static ss11_dsa_stack_queue.thuchanh.create_stack_use_class_linkedlist.MyGenericStack.stackOfIntegers;
+
+public class GenericStackTesst {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+        stackOfIStrings();
+    }
+}
